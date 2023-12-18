2389ee6f5f0c8fb0482db9cba3d89b8ba512f4e4
ChaituKReddy <chaitanyakr97@gmail.com>
2023-12-18 18:27:18 +0530

    Added new networks

---
+++ networks/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  ethereum: {
+    derivationPath: "m/44'/60'/0'/0/",
+  },
+  bitcoin: {
+    derivationPath: "m/44'/0'/0'/0/",
+  },
+  solana: {
+    derivationPath: "m/44'/501'/0'/",
+  },
+  tezos: {
+    derivationPath: "m/44'/1729'/0'/",
+  },
+  litecoin: {
+    derivationPath: "m/84'/2'/0'/0/",
+  },
+  dogecoin: {
+    segwitDerivationPath: "m/49'/3'/0'/0/",
+    derivationPath: "m/44'/3'/0'/0/",
+  },
+};
